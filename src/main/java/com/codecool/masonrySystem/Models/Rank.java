58bc94c53622994cd65deb067601ba243b3e6d35
Alek <sforaaleksander@gmail.com>
2020-07-20 20:58:07 +0200

    implement Rank enum

---
+++ src/main/java/com/codecool/masonrySystem/Models/Rank.java
@@ -0,0 +1,22 @@
+package com.codecool.masonrySystem.Models;
+
+public enum Rank {
+
+    BLUELODGE("Blue Lodge"),
+    THEROYALARCH("The Royal Arch"),
+    GRANDSUVERENINSPECTORS33RDDEGREE("Grand Suveren Inspectors 33rd Degree"),
+    SUPREMECOUNCILOFGRANDINSPECTORS("Supreme Council Of Grand Inspectors"),
+    THEORDEROFTHETRAPEZOID("The Order Of The Trapezoid"),
+    ANCIENTRITES("Ancient Rites"),
+    ORDOTEMPLIORIENTIS("Ordo Templis Orientis"),
+    THEPALLADIUM("The Palladium"),
+    THEILUMINATI("The Iluminati"),
+    AINSOPHAUR("Ain Soph Aur");
+
+    String rankString;
+
+
+    Rank(String rankString){
+        this.rankString = rankString;
+    }
+}
