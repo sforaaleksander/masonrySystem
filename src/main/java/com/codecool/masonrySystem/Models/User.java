d92b7264e76838bea6e4b556c8808ff53f9d42b3
Alek <sforaaleksander@gmail.com>
2020-07-20 20:58:35 +0200

    create abstract user class

---
+++ src/main/java/com/codecool/masonrySystem/Models/User.java
@@ -0,0 +1,58 @@
+package com.codecool.masonrySystem.Models;
+
+public abstract class User {
+    String login;
+    String password;
+    String email;
+    String firstName;
+    String lastName;
+    Rank rank;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+}
