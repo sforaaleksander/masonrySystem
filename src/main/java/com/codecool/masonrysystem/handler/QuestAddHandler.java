ba39d9e0cce320cfd85324c892e5ba42fddcfa9c
Alek <sforaaleksander@gmail.com>
2020-08-20 22:32:32 +0200

    building up set up for adding quest to db

--- src/main/java/com/codecool/masonrysystem/handler/QuestAddHandler.java
+++ src/main/java/com/codecool/masonrysystem/handler/QuestAddHandler.java
@@ -5,10 +5,13 @@ import com.codecool.masonrysystem.dao.SessionDao;
 import com.codecool.masonrysystem.dao.UserDao;
 import com.codecool.masonrysystem.helper.CookieHelper;
 import com.codecool.masonrysystem.model.Quest;
+import com.codecool.masonrysystem.model.Rank;
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class QuestAddHandler extends Handler<Quest> implements HttpHandler {
     public QuestAddHandler(CookieHelper cookieHelper, SessionDao sessionDao, UserDao userDao) {
@@ -17,7 +20,31 @@ public class QuestAddHandler extends Handler<Quest> implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        String response = createResponse();
-        send200(httpExchange, response);
+        String method = httpExchange.getRequestMethod();
+        if (method.equals("GET")) {
+            String response = createResponse();
+            send200(httpExchange, response);
+        } else {
+            System.out.println("posting");
+            postForm(httpExchange);
+        }
+    }
+
+    private void postForm(HttpExchange httpExchange) throws IOException {
+        Map<String, String> inputs = getInputs(httpExchange);
+        Quest quest = new Quest();
+        quest.setName(inputs.get("quest-name"));
+        quest.setReward(Integer.valueOf(inputs.get("quest-reward")));
+        quest.setRequiredRank(Rank.THEILLUMINATI);
+        quest.setDescription(inputs.get("quest-description"));
+        quest.setExpirationDate(null);
+        quest.setIsActive(true);
+        quest.setIsCollective(false);
+        dao.insert(quest);
+        System.out.println("inserted quest to db");
+        Headers responseHeaders = httpExchange.getResponseHeaders();
+        responseHeaders.set("Location", "academy");
+        httpExchange.sendResponseHeaders(302, -1);
+        httpExchange.close();
     }
 }
