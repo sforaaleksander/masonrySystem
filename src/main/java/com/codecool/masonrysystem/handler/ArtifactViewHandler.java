333da431806725d2dbd7bbe9033cfd802fa5fa6f
Alek <sforaaleksander@gmail.com>
2020-08-20 06:57:00 +0200

    implement ArtifactViewHandler

---
+++ src/main/java/com/codecool/masonrysystem/handler/ArtifactViewHandler.java
@@ -0,0 +1,21 @@
+package com.codecool.masonrysystem.handler;
+
+import com.codecool.masonrysystem.dao.ArtifactDao;
+import com.codecool.masonrysystem.dao.SessionDao;
+import com.codecool.masonrysystem.helper.CookieHelper;
+import com.codecool.masonrysystem.model.Artifact;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import java.io.IOException;
+
+public class ArtifactViewHandler extends Handler<Artifact> implements HttpHandler {
+
+    public ArtifactViewHandler(CookieHelper cookieHelper, SessionDao sessionDao) {
+        super("artifact_details.twig", cookieHelper, null, sessionDao, new ArtifactDao());
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        superHandleGetSingle(httpExchange);
+    }
+}
