8b8606e5d4a9e20d1a0ae9f53b195940582bbe59
Jasio <mcyanek@gmail.com>
2020-08-31 13:35:09 +0200

    updates pom dependencies

---
+++ src/test/java/com/codecool/masonrysystem/handler/LoginHandlerTest.java
@@ -0,0 +1,22 @@
+package com.codecool.masonrysystem.handler;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginHandlerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void handle() {
+    }
+}
