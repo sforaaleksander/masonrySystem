3f88eb22a56193c26730a19fccfd166da1d089cf
Maciek <macardorpl@gmail.com>
2020-08-31 11:16:28 +0200

    mockito and junit dependencies

---
+++ src/test/java/com/codecool/masonrysystem/dao/ArtifactDaoTest.java
@@ -0,0 +1,17 @@
+package com.codecool.masonrysystem.dao;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.ResultSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArtifactDaoTest {
+    ArtifactDao artifactDao = new ArtifactDao();
+
+    @Test
+    public void testIsArtifactPresent(){
+        ResultSet resultSetMock = mock()
+    }
+}
