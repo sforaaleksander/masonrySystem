1d7997ab35a9349350119233fe9a672285cc572f
Jasio <mcyanek@gmail.com>
2020-09-01 19:38:41 +0200

    implements getAllElements test in SessionDaoTest

--- src/test/java/com/codecool/masonrysystem/dao/SessionDaoTest.java
+++ src/test/java/com/codecool/masonrysystem/dao/SessionDaoTest.java
@@ -1,33 +1,65 @@
 package com.codecool.masonrysystem.dao;
 
+import com.codecool.masonrysystem.helper.IdGenerator;
+import com.codecool.masonrysystem.model.*;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.stub;
 
 class SessionDaoTest {
     static SessionDao sessionDao;
+    static Journeyman journeyman;
+    static MasterMason masterMason;
+    static Apprentice apprentice;
 
-    @BeforeEach
-    void setUp() {
+    @BeforeAll
+    public static void init() {
         sessionDao = new SessionDao();
+        apprentice = mock(Apprentice.class);
+        journeyman = mock(Journeyman.class);
+        masterMason = mock(MasterMason.class);
     }
 
-    @AfterEach
-    void tearDown() {
-        sessionDao = null;
+    @Test
+    void insert() {
+        String sessionId = (new IdGenerator()).generateId(16);
+        Session session = mock(Session.class);
+        stub(session.getSessionId()).toReturn(sessionId);
+        stub(session.getUserId()).toReturn((long)13);
+        try {
+            assertTrue(sessionDao.insert(session));
+            assertEquals(13, sessionDao.getById(sessionId).getUserId());
+            sessionDao.delete(sessionId);
+        } catch (SQLException throwables) {
+            assertTrue(false);
+            throwables.printStackTrace();
+        }
     }
-
-//    @Test
-//    void getConnection() {
-//
-//    }
 
     @Test
     void getAllElements() {
+        try {
+            HashSet<Session> sessions = new HashSet<Session>();
+            sessionDao.getAllElements().stream().forEach(s -> {
+                assertEquals("class com.codecool.masonrysystem.model.Session", s.getClass().toString());
+                assertNotNull(s.getUserId());
+                assertNotNull(s.getSessionId());
+                assertFalse(sessions.contains(s));
+                sessions.add(s);
+            });
+        } catch (SQLException throwables) {
+            assertTrue(false);
+            throwables.printStackTrace();
+        }
     }
 
     @Test
@@ -58,9 +90,7 @@ class SessionDaoTest {
     void testGetById() {
     }
 
-    @Test
-    void insert() {
-    }
+
 
     @Test
     void update() {
