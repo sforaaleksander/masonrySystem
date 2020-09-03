145c1780b17b9ae0eee52f8e42a83b8a6a65954a
Maciek <macardorpl@gmail.com>
2020-09-03 13:21:22 +0200

    UserDaoTest mock refactor, making tests independent

--- src/test/java/com/codecool/masonrysystem/dao/UserDaoTest.java
+++ src/test/java/com/codecool/masonrysystem/dao/UserDaoTest.java
@@ -4,7 +4,9 @@ import com.codecool.masonrysystem.exception.ElementNotFoundException;
 import com.codecool.masonrysystem.model.Apprentice;
 import com.codecool.masonrysystem.model.Rank;
 import com.codecool.masonrysystem.model.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,37 +14,43 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class UserDaoTest {
-    private UserDao userDao = new UserDao();
+    private UserDao userDao;
+    private User userMock;
+
+    @BeforeAll
+    public void setUp(){
+        userDao = new UserDao();
+        userMock = mock(Apprentice.class);
+        stub(userMock.getId()).toReturn(999L);
+        stub(userMock.getFirstName()).toReturn("TestFirstName");
+        stub(userMock.getLastName()).toReturn("TestLastName");
+        stub(userMock.getEmail()).toReturn("email@email.com");
+        stub(userMock.getPassword()).toReturn("password");
+        stub(userMock.getRank()).toReturn(Rank.AINSOPHAUR);
+        stub(userMock.getIsActive()).toReturn(true);
+    }
 
     @Test
     public void testIsUserPresentFromResultSet() throws SQLException {
         ResultSet resultSetMock = mock(ResultSet.class);
-        stub(resultSetMock.getLong("id")).toReturn(1L);
-        stub(resultSetMock.getString("first_name")).toReturn("first_name");
-        stub(resultSetMock.getString("last_name")).toReturn("last_name");
-        stub(resultSetMock.getString("email")).toReturn("email");
+        stub(resultSetMock.getLong("id")).toReturn(999L);
+        stub(resultSetMock.getString("first_name")).toReturn("TestFirstName");
+        stub(resultSetMock.getString("last_name")).toReturn("TestLastName");
+        stub(resultSetMock.getString("email")).toReturn("email@email.com");
         stub(resultSetMock.getString("password")).toReturn("password");
         stub(resultSetMock.getBoolean("is_active")).toReturn(true);
-
-
-        User userExpected = new Apprentice();
-        userExpected.setId(1L);
-        userExpected.setFirstName("first_name");
-        userExpected.setLastName("last_name");
-        userExpected.setEmail("email");
-        userExpected.setPassword("password");
-        userExpected.setIsActive(true);
 
         User user = userDao.create(resultSetMock);
 
         assertAll("Should return complete object",
-                () -> assertEquals(userExpected.getId(), user.getId()),
-                () -> assertEquals(userExpected.getFirstName(), user.getFirstName()),
-                () -> assertEquals(userExpected.getLastName(), user.getLastName()),
-                () -> assertEquals(userExpected.getEmail(), user.getEmail()),
-                () -> assertEquals(userExpected.getPassword(), user.getPassword()),
-                () -> assertEquals(userExpected.getIsActive(), user.getIsActive())
+                () -> assertEquals(userMock.getId(), user.getId()),
+                () -> assertEquals(userMock.getFirstName(), user.getFirstName()),
+                () -> assertEquals(userMock.getLastName(), user.getLastName()),
+                () -> assertEquals(userMock.getEmail(), user.getEmail()),
+                () -> assertEquals(userMock.getPassword(), user.getPassword()),
+                () -> assertEquals(userMock.getIsActive(), user.getIsActive())
         );
     }
 
@@ -53,14 +61,6 @@ class UserDaoTest {
 
     @Test
     public void testIsUserInserting() throws SQLException {
-        Apprentice userMock = mock(Apprentice.class);
-        stub(userMock.getId()).toReturn(999L);
-        stub(userMock.getFirstName()).toReturn("First Name");
-        stub(userMock.getLastName()).toReturn("Last Name");
-        stub(userMock.getEmail()).toReturn("email@email.com");
-        stub(userMock.getPassword()).toReturn("password");
-        stub(userMock.getRank()).toReturn(Rank.AINSOPHAUR);
-        stub(userMock.getIsActive()).toReturn(true);
         userDao.insert(userMock);
         User user = userDao.getById(999L);
         assertAll("Should return complete object",
@@ -71,43 +71,52 @@ class UserDaoTest {
                 () -> assertEquals(userMock.getPassword(), user.getPassword()),
                 () -> assertEquals(userMock.getIsActive(), user.getIsActive())
         );
+        userDao.delete(999L);
     }
 
     @Test
     public void testIsElementPresentById() throws SQLException {
-        assertNotNull(userDao.getById(1L));
+        userDao.insert(userMock);
+        assertNotNull(userDao.getById(999L));
+        userDao.delete(999L);
     }
 
 
     @Test
     public void testIsUserUpdating() throws SQLException {
-        User userMock = mock(User.class);
-        stub(userMock.getId()).toReturn(999L);
-        stub(userMock.getFirstName()).toReturn("Updated First Name");
-        stub(userMock.getLastName()).toReturn("Updated Last Name");
-        stub(userMock.getEmail()).toReturn("email@email.com");
-        stub(userMock.getPassword()).toReturn("password");
-        stub(userMock.getRank()).toReturn(Rank.AINSOPHAUR);
-        stub(userMock.getIsActive()).toReturn(true);
-        userDao.update(userMock);
+        User userMockUpdated = mock(User.class);
+        stub(userMockUpdated.getId()).toReturn(999L);
+        stub(userMockUpdated.getFirstName()).toReturn("Updated First Name");
+        stub(userMockUpdated.getLastName()).toReturn("Updated Last Name");
+        stub(userMockUpdated.getEmail()).toReturn("email@email.com");
+        stub(userMockUpdated.getPassword()).toReturn("password");
+        stub(userMockUpdated.getRank()).toReturn(Rank.THEILLUMINATI);
+        stub(userMockUpdated.getIsActive()).toReturn(true);
+
+        userDao.insert(userMock);
+        userDao.update(userMockUpdated);
         User user = userDao.getById(999L);
         assertAll("Should return complete object",
-                () -> assertEquals(userMock.getId(), user.getId()),
-                () -> assertEquals(userMock.getFirstName(), user.getFirstName()),
-                () -> assertEquals(userMock.getLastName(), user.getLastName()),
-                () -> assertEquals(userMock.getEmail(), user.getEmail()),
-                () -> assertEquals(userMock.getPassword(), user.getPassword()),
-                () -> assertEquals(userMock.getIsActive(), user.getIsActive())
+                () -> assertEquals(userMockUpdated.getId(), user.getId()),
+                () -> assertEquals(userMockUpdated.getFirstName(), user.getFirstName()),
+                () -> assertEquals(userMockUpdated.getLastName(), user.getLastName()),
+                () -> assertEquals(userMockUpdated.getEmail(), user.getEmail()),
+                () -> assertEquals(userMockUpdated.getPassword(), user.getPassword()),
+                () -> assertEquals(userMockUpdated.getIsActive(), user.getIsActive())
         );
+        userDao.delete(999L);
     }
 
     @Test
     public void testIsUserPresentByEmail() throws SQLException {
+        userDao.insert(userMock);
         assertEquals("email@email.com", userDao.getUserByEmail("email@email.com").getEmail());
+        userDao.delete(999L);
     }
 
     @Test
     public void testIsUserDeleting(){
+        userDao.insert(userMock);
         userDao.delete(999L);
         assertThrows(ElementNotFoundException.class, () -> userDao.getById(999L));
     }
