4cae38cb1280541ccbd1039bc4157e4256694172
Dominik <domek99@gmail.com>
2020-09-03 11:14:50 +0200

    clean code and try to add more tests

--- src/test/java/com/codecool/masonrysystem/dao/LodgeDaoTest.java
+++ src/test/java/com/codecool/masonrysystem/dao/LodgeDaoTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.TestInstance;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
@@ -16,17 +18,19 @@ import static org.mockito.Mockito.*;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class LodgeDaoTest {
 
-    private LodgeDao lodgeDao;
+    private LodgeDao lodgeDaoWithMock;
     private Journeyman journeymanMock;
     private ResultSet resultSetMock;
     private Lodge expectedLodge;
     private Lodge lodge;
+//    private LodgeDao lodgeDao = new LodgeDao();
 
     @BeforeAll
     public void setUp() throws SQLException {
         UserDao userDaoMock = mock(UserDao.class);
         resultSetMock = mock(ResultSet.class);
-        lodgeDao = new LodgeDao(userDaoMock);
+        lodgeDaoWithMock = new LodgeDao(userDaoMock);
+
         journeymanMock = mock(Journeyman.class, RETURNS_MOCKS);
         when(userDaoMock.getById(any())).thenReturn(journeymanMock);
         when(resultSetMock.getLong("id")).thenReturn(0L);
@@ -43,19 +47,24 @@ class LodgeDaoTest {
     }
 
     private void createLodgeFromResultSet() throws SQLException {
-        lodge = lodgeDao.create(resultSetMock);
+        lodge = lodgeDaoWithMock.create(resultSetMock);
     }
 
     @Test
     public void testIfIdsAreSameInExpectedAndCreatedLodge() {
-        assertEquals(expectedLodge.getId(), lodge.getId());
+        assertAll("Check if expectedLodge consistent with lodge",
+                () -> assertEquals(expectedLodge.getId(), lodge.getId()),
+                () -> assertEquals(expectedLodge.getName(), lodge.getName()),
+                () -> assertEquals(expectedLodge.getOwner(), lodge.getOwner()
+                ));
+    }
+
+    @Test
+    public void testIfAllElementsArePresent() throws SQLException {
+        assertNotNull(lodgeDao.getAll());
     }
     @Test
-    public void testIfNamesAreSameInExpectedAndCreatedLodge() {
-        assertEquals(expectedLodge.getName(), lodge.getName());
-    }
-    @Test
-    public void testIfOwnersAreSameInExpectedAndCreatedLodge() {
-        assertEquals(expectedLodge.getOwner(), lodge.getOwner());
+    public void testIfElementByIdIsPresent() throws SQLException {
+        assertNotNull(lodgeDao.getById(1L));
     }
 }
